d00acceacfd9af4554e2561b8450004464215d9a
赵楠 <2205567926@qq.com>
2017-06-01 20:38:59 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -1,2 +1,9 @@
 // Write your code here
 module.exports = Person
+var Person = function(name ,age){
+    this.name= name,
+    this.age= age,
+    this.introduce= function() {
+        return "My name is "+ this.name+". I am "+this.age+" years old.";
+    }
+}
