532d211abb2138a3265741d6b4051e07f4908e0e
赵楠 <2205567926@qq.com>
2017-06-01 20:39:38 +0800

    Update student.js

--- main/student.js
+++ main/student.js
@@ -1,3 +1,11 @@
 let Person = require('./person')
 // Write your code here
+function Student (name, age, Class){
+    this.class= 2,
+    this.name = "朱一百",
+    this.age = "100",
+    this.introduce = function(){
+        return "My name is Tom. I am 21 years old. I am a Student. I am at Class 2.";
+    }
+}
 module.exports = Student
