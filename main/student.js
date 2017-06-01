75989286532725066d4b5a7336133cb35f935ea6
DESKTOP-NLMFL51\ZN <2205567926@qq.com>
2017-06-01 20:50:07 +0800

    no message

--- main/student.js
+++ main/student.js
@@ -3,10 +3,10 @@ let Person = require('./person')
 function Student (name, age, Class){
     Person.call(this);
     this.klass= Class;
-    this.name = naem;
+    this.name = name;
     this.age = age;
     this.introduce = function(){
-        return "My name is "+this.name+". I am 21 "+this.age+"years old. I am a Student. I am at Class "+this.klass+"2.";
+        return "My name is "+this.name+". I am "+this.age+" years old. I am a Student. I am at Class "+this.klass+".";
     }
 }
 module.exports = Student
